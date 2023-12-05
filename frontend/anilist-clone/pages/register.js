ce20cc366e0835a74ec94ef4661100055e96fdf8
Yassine El Abdellati <yassineelab@hotmail.com>
2023-12-05 15:45:54 +0100

    register page

--- frontend/anilist-clone/pages/register.js
+++ frontend/anilist-clone/pages/register.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { Navbar, Footer } from "@/app/components/imports";
 
-const register = () => {
-  return <div>register</div>;
+const Register = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center h-screen bg-[#0a1625]">
+        <div className="container-loginpage w-96 bg-[#152232] p-8 rounded shadow-md h-3/5 flex flex-col justify-between items-center">
+          <div>
+            <h1 className="text-2xl font-bold mb-16 items-center text-center">
+              Sign up to AniList
+            </h1>
+            <form className="flex flex-col items-center">
+              <input
+                type="text"
+                placeholder="Email"
+                className="w-full p-2 mb-4 border border-gray-300 rounded bg-[#0a1625]"
+              />
+              <input
+                type="text"
+                placeholder="Username"
+                className="w-full p-2 mb-4 border border-gray-300 rounded bg-[#0a1625]"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full p-2 mb-4 border border-gray-300 rounded bg-[#0a1625]"
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                className="w-full p-2 mb-4 border border-gray-300 rounded bg-[#0a1625]"
+              />
+              <button
+                type="submit"
+                className="w-1/3 p-2 bg-blue-500 text-white rounded"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
-export default register;
+export default Register;
